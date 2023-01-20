11608f169fe98944b26c22850bbb707d1b6e587b
antalandreiu <antalandreiu@gmail.com>
2023-01-20 15:28:27 +0100

    changed format of json file

--- scripts/main.py
+++ scripts/main.py
@@ -35,7 +35,7 @@ def forecast(region: str, province: str, type_of_exercise: str, tourist_residenc
 
     data = [{"date": indexes[i].strftime("%Y-%m"),  "npeople": values[i]} for i in range(len(values))]
 
-    return str({"prediction": data})
+    return {"prediction": data}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
